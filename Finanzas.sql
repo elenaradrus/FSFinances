279e34670b09f508ec7baa640a53f84ae114baf7
Elena Radu <lena@Air-de-Elena.home>
2022-07-08 19:54:41 +0200

    sending info from DB into finance view

--- Finanzas.sql
+++ Finanzas.sql
@@ -16,22 +16,29 @@ SELECT * FROM Usuarios;
 
 CREATE TABLE Finanzas(
     id INT AUTO_INCREMENT,
+    fk_id_usuario INT NOT NULL,
     ingreso VARCHAR(100) NOT NULL,
     ahorroEsperado VARCHAR(100) NOT NULL,
     gastoTotal VARCHAR(100),
     ahorroReal VARCHAR(100),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Usuarios_Finanzas(
-	id INT AUTO_INCREMENT,
-	fk_id_usuario INT NOT NULL,
-	fk_id_finanzas INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id) ON UPDATE CASCADE ON DELETE CASCADE, 
-	FOREIGN KEY(fk_id_finanzas) REFERENCES Finanzas(id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+#INSERT INTO Finanzas VALUES (null, 1, "1500", "400",null,null);
 
+select * from Finanzas where fk_id_usuario = 1;
+
+SELECT * FROM Finanzas;
+
+#CREATE TABLE Usuarios_Finanzas(
+#	id INT AUTO_INCREMENT,
+#	fk_id_usuario INT NOT NULL,
+#	fk_id_finanzas INT NOT NULL,
+#	PRIMARY KEY (id),
+#	FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id) ON UPDATE CASCADE ON DELETE CASCADE, 
+#	FOREIGN KEY(fk_id_finanzas) REFERENCES Finanzas(id) ON UPDATE CASCADE ON DELETE CASCADE
+#);
 
 CREATE TABLE GastosHogar (
     id INT AUTO_INCREMENT,
